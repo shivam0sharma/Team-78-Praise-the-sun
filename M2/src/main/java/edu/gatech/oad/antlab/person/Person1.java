871f16daa4460308ba9dcf7bbe9e22c9d61ca322
ameyavitankar <ameyavitankar@gmail.com>
2018-09-06 17:05:30 -0400

    changed p1

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,9 +3,9 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
+ *  modified string
+ *
+ *  @author Ameya Vitankar
  *  @version 1.1
  */
 public class Person1 {
@@ -31,15 +31,17 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+    int offs = 2 % input.length();
+		return input.substring(offs) + input.substring(0, offs);
+
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
