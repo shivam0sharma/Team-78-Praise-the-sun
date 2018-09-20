cd2d48df5c39098a571c6737a9e247caa382d96a
Shaisan2 <shivam0sharma@icloud.com>
2018-09-20 16:59:43 -0400

    Created project

---
+++ Donation Tracker/app/src/main/java/com/team78/praise_the_sun/donationtracker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.team78.praise_the_sun.donationtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
